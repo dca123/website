39d930730b0ea44d9573833701a8c308497256e4
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-01-15 01:49:49 +0530

    feat: :sparkles: Added font optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import "@fontsource/montserrat/800.css";
-import "@fontsource/montserrat/700.css";
-import "@fontsource/montserrat/600.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/400.css";
+// import "@fontsource/montserrat/800.css";
+// import "@fontsource/montserrat/700.css";
+// import "@fontsource/montserrat/600.css";
+// import "@fontsource/montserrat/500.css";
+// import "@fontsource/montserrat/400.css";
 
-import "@fontsource/nunito/200.css";
-import "@fontsource/nunito/300.css";
-import "@fontsource/nunito/400.css";
-import "@fontsource/nunito/500.css";
-import "@fontsource/nunito/600.css";
-import "@fontsource/nunito/700.css";
-import "@fontsource/nunito/800.css";
-import "@fontsource/nunito/900.css";
+// import "@fontsource/nunito/200.css";
+// import "@fontsource/nunito/300.css";
+// import "@fontsource/nunito/400.css";
+// import "@fontsource/nunito/500.css";
+// import "@fontsource/nunito/600.css";
+// import "@fontsource/nunito/700.css";
+// import "@fontsource/nunito/800.css";
+// import "@fontsource/nunito/900.css";
 
 import { theme } from "../theme";
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,12 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Nunito:ital,wght@0,300;0,400;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
