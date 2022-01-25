5854b51390a8dba8e721283142edbed6c5f64721
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-01-25 16:56:59 +0530

    feat(_app): Added device sizes to only upto 1920

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images:{
-    domains: ["picsum.photos", "images.unsplash.com", "source.unsplash.com", "s3.us-west-2.amazonaws.com"]
+    domains: ["images.unsplash.com", "source.unsplash.com", "s3.us-west-2.amazonaws.com"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   }
 }
