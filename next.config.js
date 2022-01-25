3a46cf3795dbecaf06cd9be99c2f16ca204cbc2d
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-01-25 23:42:25 +0530

    perf: :zap: Improves speed on image loading with plaiceholder

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withPlaiceholder } = require("@plaiceholder/next");
+
+module.exports = withPlaiceholder({
+  /** @type {import('next').NextConfig} */
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "source.unsplash.com", "s3.us-west-2.amazonaws.com"],
+    domains: [
+      "images.unsplash.com",
+      "source.unsplash.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-  }
-}
+  },
+});
