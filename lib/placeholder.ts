6ac4edf6ab19dfb2a2943e764be9f7096599e897
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-01-28 18:07:34 +0530

    fix: :bug: Fixes placeholder only bluring for prod

--- lib/placeholder.ts
+++ lib/placeholder.ts
@@ -5,7 +5,7 @@ export const getBase64PlaceHolder = async (
   imageURL: string,
   blurSize: number
 ) => {
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV === "production") {
     return (await getPlaiceholder(imageURL, { size: blurSize })).base64;
   } else {
     return "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z";
