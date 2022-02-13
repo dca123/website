c6fae00588d1957567bf19c37f4a82128fbfc83f
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-02-13 22:29:06 +0530

    feat: reduce bluesize to 6

--- lib/placeholder.ts
+++ lib/placeholder.ts
@@ -3,7 +3,7 @@ import { getPlaiceholder } from "plaiceholder";
 // Disables getPlaiceholder optimizations on local development
 export const getBase64PlaceHolder = async (imageURL: string) => {
   if (process.env.NODE_ENV === "production") {
-    return (await getPlaiceholder(imageURL, { size: 10 })).base64;
+    return (await getPlaiceholder(imageURL, { size: 6 })).base64;
   } else {
     return "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z";
   }
